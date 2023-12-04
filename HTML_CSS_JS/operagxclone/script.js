8733acd8f2a959949df0c149bf80073f825138c0
MSCWCollegeApp <mannyk778@gmail.com>
2023-12-04 05:30:35 +0530

    ui improved and finished

---
+++ HTML_CSS_JS/operagxclone/script.js
@@ -0,0 +1,17 @@
+document.getElementById("gamec11").style = "background-image: linear-gradient( rgba(21, 7, 34,0), rgba(21, 7, 34,1)), url('https://store-images.s-microsoft.com/image/apps.442.14492077886571533.b0266c29-bb30-438c-b201-6b7e235b7920.6ebc94e7-3c85-4277-bee7-bdb2b8ef12dc?q=90&w=256&h=384&mode=crop&format=jpg&background=%23FFFFFF')"
+document.getElementById("gamec12").style = "background-image: linear-gradient( rgba(21, 7, 34,0), rgba(21, 7, 34,1)), url('https://m.media-amazon.com/images/I/918f0DFppPL._AC_UF894,1000_QL80_.jpg')"
+document.getElementById("gamec13").style = "background-image: linear-gradient( rgba(21, 7, 34,0), rgba(21, 7, 34,1)), url('https://i.redd.it/07n23s1vxjw01.jpg')"
+document.getElementById("gamec14").style = "background-image: linear-gradient( rgba(21, 7, 34,0), rgba(21, 7, 34,1)), url('https://m.media-amazon.com/images/I/71rbf1GfogL._AC_UF894,1000_QL80_.jpg')"
+document.getElementById("gamec15").style = "background-image: linear-gradient( rgba(21, 7, 34,0), rgba(21, 7, 34,1)), url('https://m.media-amazon.com/images/I/71bQrBXpk9L._AC_UF894,1000_QL80_.jpg')"
+document.getElementById("gamec16").style = "background-image: linear-gradient( rgba(21, 7, 34,0), rgba(21, 7, 34,1)), url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPx0eNdGlRMP4K2gcPk4_otJUlG2LlZNOUr--piwklCajNMUuTuYmIpYeVZZWB1-CHCOk&usqp=CAU')"
+document.getElementById("gamec17").style = "background-image: linear-gradient( rgba(21, 7, 34,0), rgba(21, 7, 34,1)), url('https://m.media-amazon.com/images/I/61wgujVHwNL._AC_UF894,1000_QL80_.jpg')"
+document.getElementById("gamec18").style = "background-image: linear-gradient( rgba(21, 7, 34,0), rgba(21, 7, 34,1)), url('https://i.pinimg.com/1200x/37/cc/f8/37ccf8176fda4aecf90322ac7bfd71cd.jpg')"
+document.getElementById("gamec19").style = "background-image: linear-gradient( rgba(21, 7, 34,0), rgba(21, 7, 34,1)), url('https://m.media-amazon.com/images/I/51dad+XhDDL.jpg')"
+
+
+
+document.getElementById("gamec21").style = "background-image: linear-gradient( rgba(50, 47, 53, 1),rgba(50, 47, 53,0)), url('https://img.itch.zone/aW1nLzEyMzA0NjgwLnBuZw==/original/Y1Th15.png')"
+document.getElementById("gamec22").style = "background-image: linear-gradient( rgba(50, 47, 53, 1),rgba(50, 47, 53,0)), url('https://img.itch.zone/aW1hZ2UvNjk3MzgzLzM4NTAzNTIucG5n/347x500/9ka%2BVU.png')"
+document.getElementById("gamec23").style = "background-image: linear-gradient( rgba(50, 47, 53, 1),rgba(50, 47, 53,0)), url('https://cdn.akamai.steamstatic.com/steam/apps/1561670/header.jpg?t=1694173631')"
+document.getElementById("gamec24").style = "background-image: linear-gradient( rgba(50, 47, 53, 1),rgba(50, 47, 53,0)), url('https://geometrydash.io/data/image/game/spacebar-clicker.png')"
+document.getElementById("gamec25").style = "background-image: linear-gradient( rgba(50, 47, 53, 1),rgba(50, 47, 53,0)), url('https://img.itch.zone/aW1nLzExOTIwMzQ3LnBuZw==/original/Kkkmqo.png')"
