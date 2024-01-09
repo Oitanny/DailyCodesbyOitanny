98bc0bf6b3068cad2caeb8bfbe0c032fa1ea8d8d
MSCWCollegeApp <mannyk778@gmail.com>
2024-01-09 23:48:01 +0530

    class concept

---
+++ C++/hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    //this program illustrates the class concept and inheritance concept in c++
+    cout<<"Hello this is a sample text";
+
+    return 0;
+}
